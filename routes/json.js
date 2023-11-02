6a54134be8c0052118b26b6ad56f03ce7cbc2636
easyevi <andreas.gloeer@gmail.com>
2023-11-02 09:34:47 +0100

    testing

--- routes/json.js
+++ routes/json.js
@@ -4,13 +4,14 @@ var jsend = require('jsend');
 router.use(jsend.middleware);
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
-
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json() 
 
 router.get('/', async function(req, res, next) {
   res.jsend('this is the json endpoint');
 });
 
-router.post('/', async function(req, res, next) {
+router.post('/', jsonParser, async function(req, res, next) {
   let input = req.body; 
   console.log(input);
 });
