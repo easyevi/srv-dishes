6a6fbefae61272d897c57e983382e3ff5307c05b
easyevi <andreas.gloeer@gmail.com>
2023-11-02 11:46:32 +0100

    dishes

--- routes/dishes.js
+++ routes/dishes.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
+let dishes = db.collection('dishes')
 
 
 router.get('/', async function(req, res, next) {
-  res.send('this is the dishes endpoint');
+  let list = await dishes.list();
+  res.send(list);
 });
 
 router.get('/:key', async function(req, res, next) {
-  res.send('respond with a resource');
+  let item = await dishes.get(req.params.key);
+  res.send(item);
 });
 
 router.post('/', async function(req, res, next) {
-  res.send('respond with a resource');
+  const {name, country} = req.body;
+  await dishes.set(name, {
+    country: country,
+  })
+  res.end();
 });
 
 module.exports = router;
