86a5d8f23ef8f8582f9c12a914745fa4489a5d97
easyevi <andreas.gloeer@gmail.com>
2023-11-02 09:26:36 +0100

    json post test

--- routes/json.js
+++ routes/json.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var jsend = require('jsend');
 router.use(jsend.middleware);
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 
 router.get('/', async function(req, res, next) {
@@ -9,7 +11,8 @@ router.get('/', async function(req, res, next) {
 });
 
 router.post('/', async function(req, res, next) {
-  res.send('respond with a resource');
+  let input = req.body; 
+  console.log(input);
 });
 
 module.exports = router;
